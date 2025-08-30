6034042668677a50ae86599bcf90a463e084df16
michelemadonna <michele.madonna@w3studio.it>
2025-08-30 11:46:19 +0200

    Fix symlink check for .p10k.zsh and clean up comments in oh-my-posh setup scripts

--- oh-my-posh/start_oh_my_posh.zsh
+++ oh-my-posh/start_oh_my_posh.zsh
@@ -1,12 +1,10 @@
 #!/usr/bin/env zsh
-local DOTFILES_DIR="${0:A:h:h}"
-
-#verifica se esiste il comando oh-my-posh. se non esiste esci e dai un messaggio in inglese suggerendo di usare $DOTFILES_DIR/Readme.md
+# this file is responsible for setting up oh-my-posh
 if ! command -v oh-my-posh &> /dev/null; then
     echo "oh-my-posh not found. Please refer to $DOTFILES_DIR/Readme.md for installation instructions."
     exit 1
 else
-    #se esiste il file omp.local.zsh
+
     if [ -f "$DOTFILES_DIR/oh-my-posh/omp.local.zsh" ]; then
         source "$DOTFILES_DIR/oh-my-posh/omp.local.zsh"
     fi
