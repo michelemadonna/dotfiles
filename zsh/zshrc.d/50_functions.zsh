8f50cfe8adf652e8d3b19a066c7cf52b3fb6668f
michelemadonna <michele.madonna@w3studio.it>
2025-09-29 09:56:32 +0200

    Fix macOS icon cache function definition: correct conditional check and rename function for clarity

--- zsh/zshrc.d/50_functions.zsh
+++ zsh/zshrc.d/50_functions.zsh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
-if [[ "$(uname)" != "Darwin" ]]; then
-  cleariconcache() {
+if [[ "$(uname)" == "Darwin" ]]; then
+  clear_macos_icon_cache() {
 
     echo "🧹 Cleaning macOS icon cache..."
     sudo rm -rf /Library/Caches/com.apple.iconservices.store >/dev/null 2>&1
