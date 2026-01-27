daec7c79d067181f490e763a8ed161ce9640d21a
michelemadonna <michele.madonna@w3studio.it>
2026-01-27 15:41:21 +0100

    Expand editor list to prevent fastfetch in more IDEs
    Prevents fastfetch from running when launched from additional editors
    and terminals by expanding the parent process exclusion list.
    Improves compatibility with environments like Fresh and Helix.

--- zsh/zshrc.d/ZZZ_leave_me_to_the_end.zsh
+++ zsh/zshrc.d/ZZZ_leave_me_to_the_end.zsh
@@ -24,7 +24,7 @@ if [[ "$ZQS_SHOW_FASTFETCH" == "true" ]]; then
     # Set up fastfetch to run only if the parent command is not an editor or IDE
     # This prevents fastfetch from running in editors like VSCode, Neovim, etc.
     parent_cmd=$(ps -o comm= -p $(ps -o ppid= -p $$))
-    if ! echo "$parent_cmd" | grep -qiE 'zed|code|micro|nvim|vim|idea|clion|goland|phpstorm|pycharm|tmux|Terminal'; then
+    if ! echo "$parent_cmd" | grep -qiE 'zed|code|micro|nvim|vim|idea|clion|goland|phpstorm|pycharm|tmux|fresh|helix|Terminal'; then
       fastfetch --pipe false
     fi
   else
